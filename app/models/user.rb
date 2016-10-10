3f2524b4d62ef56d90b57bd8768e3e4892db3fa7
Sandy <SVern99@aol.com>
2016-10-10 20:15:00 +0000

    changed title and jumbotron heading, added validation for uniqueness of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
+        validates_uniqueness_of :username
+         
   has_many :articles
   has_many :comments 
   
