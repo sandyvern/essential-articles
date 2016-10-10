8b7fe993b9d4b79d7f3ade3764c38d14fce6c13c
Sandy <SVern99@aol.com>
2016-10-10 21:04:28 +0000

    edit views to include time references

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ActiveRecord::Base
   
   belongs_to :user
   has_many :comments
+  
+  validates :title, :body, presence: true
+  validates :title, uniqueness: true, uniqueness: { message: "This article title has already been used. Please create an original title."}
+  validates :body, length: { minimum: 15 }
 end
