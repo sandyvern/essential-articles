78cfb233f34b7a6e3e5dd6b454b2322e3f673be2
Sandy <SVern99@aol.com>
2016-10-18 19:20:58 +0000

    configuring AWS S3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,17 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   
   config.action_mailer.default_url_options = { host: 'https://essential-articles-sandyvern.c9users.io' }
+  
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_region: ENV["AWS_S3_REGION"],
+    s3_credentials: {
+      s3_host_name: ENV["AWS_S3_HOST_NAME"],
+      bucket: ENV["AWS_S3_BUCKET"],
+      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+      }
+    } 
 
   
 end
